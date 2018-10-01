525cd30e083605b4afb21d36a4e737d945c1b2e8
Brian Liotti <bdliotti@gmail.com>
2018-10-01 09:49:48 -0400

    practice problems

--- minimumSwaps.js
+++ minimumSwaps.js
@@ -1,7 +1,34 @@
-i   arr                         swap (indices)
-0   [7, 1, 3, 2, 4, 5, 6]   swap (0,3)
-1   [2, 1, 3, 7, 4, 5, 6]   swap (0,1)
-2   [1, 2, 3, 7, 4, 5, 6]   swap (3,4)
-3   [1, 2, 3, 4, 7, 5, 6]   swap (4,5)
-4   [1, 2, 3, 4, 5, 7, 6]   swap (5,6)
-5   [1, 2, 3, 4, 5, 6, 7]
+const minimumSwaps = arr => {
+	let n = arr.length
+	let arrWithIndex = arr.map((val, idx) => [idx, val])
+	arrWithIndex.sort((a, b) => a[1] - b[1])
+	let vis = []
+	for (let x in arr) {
+		vis.push(false)
+	}
+	let ans = 0
+	for (let i in arr) {
+		if (vis[i] || arrWithIndex[i][0] === i) {
+			continue
+		}
+		let cycle_size = 0
+		let j = i
+		while (!vis[j]) {
+			vis[j] = true
+			j = arrWithIndex[j][0]
+			cycle_size++
+		}
+		if (cycle_size > 0) {
+			ans += cycle_size - 1
+		}
+	}
+
+	return ans
+}
+
+let arr = [1, 5, 4, 3, 2]
+let arr1 = [7, 1, 3, 2, 4, 5, 6]
+let arr2 = [1, 3, 5, 2, 4, 6, 8]
+console.log(minimumSwaps(arr))
+console.log(minimumSwaps(arr1))
+console.log(minimumSwaps(arr2))
