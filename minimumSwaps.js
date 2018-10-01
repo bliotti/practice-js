fae35cd46fce941b291aa6e6e90a1475db2189df
Brian Liotti <bdliotti@gmail.com>
2018-10-01 16:27:06 -0400

    practice problems

--- minimumSwaps.js
+++ minimumSwaps.js
@@ -1,18 +1,21 @@
 const minimumSwaps = arr => {
-	let n = arr.length
 	let arrWithIndex = arr.map((val, idx) => [idx, val])
+
 	arrWithIndex.sort((a, b) => a[1] - b[1])
+
 	let vis = []
 	for (let x in arr) {
 		vis.push(false)
 	}
+
 	let ans = 0
-	for (let i in arr) {
-		if (vis[i] || arrWithIndex[i][0] === i) {
+	for (let i = 0; i < arr.length; i++) {
+		if (vis[i] == true || arrWithIndex[i][0] === i) {
 			continue
 		}
 		let cycle_size = 0
 		let j = i
+		j
 		while (!vis[j]) {
 			vis[j] = true
 			j = arrWithIndex[j][0]
@@ -29,6 +32,7 @@ const minimumSwaps = arr => {
 let arr = [1, 5, 4, 3, 2]
 let arr1 = [7, 1, 3, 2, 4, 5, 6]
 let arr2 = [1, 3, 5, 2, 4, 6, 8]
+
 console.log(minimumSwaps(arr))
 console.log(minimumSwaps(arr1))
 console.log(minimumSwaps(arr2))
